b2f1a2f0a0585ccc74a74121cfa49b092db4fe2f
jeremy <jeremcombe@hotmail.fr>
2024-02-13 23:43:21 +0100

    helia.js

---
+++ ipfs/index.js
@@ -0,0 +1,30 @@
+import { createHelia } from "helia";
+import { unixfs } from "@helia/unixfs";
+
+const create = async () => {
+  return createHelia();
+};
+
+const addData = async (helia) => {
+  const fs = unixfs(helia);
+
+  // we will use this TextEncoder to turn strings into Uint8Arrays
+  const encoder = new TextEncoder();
+  const bytes = encoder.encode("TESTING");
+
+  // add the bytes to your node and receive a unique content identifier
+  const cid = await fs.addBytes(bytes);
+
+  return cid.toString();
+};
+
+const start = async () => {
+  try {
+    const rep = await addData(await create());
+    console.log(rep);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
