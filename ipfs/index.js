80d963b96e7a8d7a9a3f94d95227964184cef243
jeremy <jeremcombe@hotmail.fr>
2024-02-13 22:55:06 +0100

    dotenv

--- ipfs/index.js
+++ ipfs/index.js
@@ -1,24 +1,23 @@
 import { create } from '@web3-storage/w3up-client'
-
+import dotenv from 'dotenv';
 
 const createClient  = () => {
     return create(); 
 }
 
-
-const createSpace = async (name) => {
+const createSpace = async (name, client) => {
     return client.createSpace(name);
 }
 
 const login = async (log) => {
     return client.login(log);
-
 }
 
 const start = async () => {
 try {
-    await createClient();
-    const space = await createSpace();
+    const client = await createClient();
+    const log = await login(process.env.ACCOUNT)
+    const space = await createSpace("testing",client);
 
     } catch (error) {
         console.log(error);
