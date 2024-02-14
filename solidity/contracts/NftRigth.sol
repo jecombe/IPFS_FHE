12575297a5bbe21314d485f53091873834455e26
jeremy <jeremcombe@hotmail.fr>
2024-02-14 17:48:03 +0100

    add EIP712WithModifier fhevm

--- solidity/contracts/NftRigth.sol
+++ solidity/contracts/NftRigth.sol
@@ -5,24 +5,47 @@ import "fhevm/lib/TFHE.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "fhevm/abstracts/EIP712WithModifier.sol";
 
 struct Data {
     euint32 privateKey;
+    euint32 metadata;
 }
 
-contract NftRigth is ERC721, Ownable {
+contract NftRigth is ERC721, Ownable, EIP712WithModifier {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     mapping(uint => Data) private nftData;
 
-    constructor() ERC721("DigitalNftRigth", "DNR") {}
+    event NftCreate(address indexed user, uint256 indexed tokenId);
+    event ChangeMessage(address indexed user, uint256 indexed tokenId);
+    event GetData(address indexed user, uint256 indexed tokenId);
 
-    function mint() public {
+    constructor() ERC721("DigitalNftRigth", "DNR") EIP712WithModifier("Authorization token", "1") {}
+
+    modifier isOwner(uint256 tokenId) {
+        _;
+    }
+
+    function mint(bytes calldata _metadata) external {
+        euint32 data = TFHE.asEuint32(_metadata);
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
         nftData[newTokenId].privateKey = createPrivateKeys();
+        nftData[newTokenId].metadata = data;
         _safeMint(msg.sender, newTokenId);
+    }
+
+    // Fonction pour ajouter des métadonnées à un token
+    function changeData(uint256 tokenId, bytes calldata _newMetadata) external {
+        require(msg.sender == ownerOf(tokenId), "You are not the owner of this token");
+        nftData[tokenId].metadata = TFHE.asEuint32(_newMetadata);
+        emit ChangeMessage(msg.sender, tokenId);
+    }
+
+    function accessData(uint256 tokenId) public {
+        require(msg.sender == ownerOf(tokenId), "You are not the owner of this token");
     }
 
     function createPrivateKeys() internal view returns (euint32) {
