8703b9e6aafbf4d8b26544cc0ac135f1df245eb1
jeremy <jeremcombe@hotmail.fr>
2024-02-15 02:24:31 +0100

    fix

--- scripts/get-abi.js
+++ scripts/get-abi.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const getTheAbi = (name) => {
   try {
-    const dir = path.resolve(__dirname, `./solidity/artifacts/contracts/${name}.sol/${name}.json`);
+    const dir = path.resolve(__dirname, `../solidity/artifacts/contracts/${name}.sol/${name}.json`);
     const file = fs.readFileSync(dir, "utf8");
     const json = JSON.parse(file);
     const abi = json.abi;
@@ -11,7 +11,7 @@ const getTheAbi = (name) => {
     console.log(`abi`, abi);
 
     // Save the ABI to a new JSON file at the root
-    const abiFilePath = path.resolve(__dirname, `./abi/${name}-abi.json`);
+    const abiFilePath = path.resolve(__dirname, `../abi/${name}-abi.json`);
     fs.writeFileSync(abiFilePath, JSON.stringify(abi, null, 2));
 
     return abi;
